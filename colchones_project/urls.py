4c9c3516936cf7ae28dceb339afe12c6822861a2
Oscar Javier Castelblanco <lubianka01@gmail.com>
2025-08-04 18:01:15 -0500

    Configurar srvicio de archivos media para produccion

--- colchones_project/urls.py
+++ colchones_project/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
+# Servir archivos media y static tanto en desarrollo como en producción
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
